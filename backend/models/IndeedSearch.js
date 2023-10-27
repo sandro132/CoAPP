b6a58bb1e7354a1d0020445b8a7340d659cc6653
sandro132 <mianveo@hotmail.com>
2023-10-27 11:47:27 -0500

    Delete function and schema of the search API

--- backend/models/IndeedSearch.js
+++ backend/models/IndeedSearch.js
@@ -1,12 +1,3 @@
 import mongoose from "mongoose";
 
-const indeedSchema = new mongoose.Schema({
-    jobTitle: String, // Título del trabajo
-    company: String, // Nombre de la empresa
-    salary: Number, // Salario (ajusta el tipo de dato según tus necesidades)
-    // Otros campos específicos de Indeed que desees almacenar
-});
-
-const IndeedModel = mongoose.model('IndeedJob', indeedSchema);
-
-module.exports = IndeedModel;
+const indeedSchema = new mongoose.Schema({})
--- backend/models/LinkedInSearch.js
+++ backend/models/LinkedInSearch.js
@@ -1,12 +1,3 @@
 import mongoose from "mongoose";
 
-const linkedInSchema = new mongoose.Schema({
-    name: String, // Nombre del usuario o profesional
-    jobTitle: String, // Título del trabajo
-    location: String, // Ubicación
-    // Otros campos específicos de LinkedIn que desees almacenar
-});
-
-const LinkedInModel = mongoose.model('LinkedInUser', linkedInSchema);
-
-module.exports = LinkedInModel;
+const linkedInSchema = new mongoose.Schema({});
