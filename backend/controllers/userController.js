eb234884f07413ee89383808e9eed1257376cd4d
sandro132 <mianveo@hotmail.com>
2023-10-30 14:40:55 -0500

    Add generar id an change schema of the users

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,49 +1,69 @@
-import ProfesionalUser from "../models/ProfesionalUser.js"
-import AdministradorUser from "../models/AdministradorUser.js"
-import EmpresaUser from "../models/EmpresaUser.js"
-
+import {  Profesional, Empresa, Administrador } from "../models/Usuarios.js"
+import generarId from "../helpers/generarId.js"
 
 //crear const "Eleccion" que reciba el tipo de usuario que se va a crear
 
 const registrar = async (req, res) => {
-    // Evitar registros duplicados
+    // Const para verificacion de correo
     const { correo } = req.body;
-    const existeUsuario = await ProfesionalUser.findOne({ correo });
 
-    if(existeUsuario) {
-        const error = new Error("usuario ya registrados");
-        return res.status(400).json({ msg: error.message });
-    }
-
-    // Condicional para elegir el tipo de usuario
-    try {
-        const profesionalUser = new ProfesionalUser(req.body);
-        const usuarioAlmacenado = await profesionalUser.save();
-        res.json(usuarioAlmacenado);
-
-    } catch (error) {
-        console.log(error)
-    };
-
-    // try {
-        // const empresaUser = new EmpresaUser(req.body);
-        // const usuarioAlmacenado = await empresaUser.save();
-        // res.json(usuarioAlmacenado);
-    // } catch (error) {
-    //     console.log(error)
-    // };
-
-    // try {
-    //     const administradorUser = new AdministradorUser(req.body);
-    //     const usuarioAlmacenado = await administradorUser.save();
-    //     res.json(usuarioAlmacenado);
-    // } catch (error) {
-    //     console.log(error)
-    // };
-
+    //Verificar el tipo de usuario
+    const { tipoUsuario } = req.body;
     
-}
+    if(tipoUsuario == "profesional") {
+        // Evitar registros duplicados
+        const existeUsuario = await Profesional.findOne({ correo });
 
+        if(existeUsuario) {
+            const error = new Error("usuario ya registrados");
+            return res.status(400).json({ msg: error.message });
+        };
+
+        try {
+            const profesionalUser = new Profesional(req.body);
+            profesionalUser.token = generarId();
+            const usuarioAlmacenado = await profesionalUser.save();
+            res.json(usuarioAlmacenado);
+        } catch (error) {
+            console.log(error)
+        };
+
+    } else if(tipoUsuario == "empresa") {
+        // Evitar registros duplicados
+        const existeUsuario = await Empresa.findOne({ correo });
+
+        if(existeUsuario) {
+            const error = new Error("usuario ya registrados");
+            return res.status(400).json({ msg: error.message });
+        };
+        
+        try {
+            const empresaUser = new Empresa(req.body);
+            empresaUser.token = generarId(); 
+            const usuarioAlmacenado = await empresaUser.save();
+            res.json(usuarioAlmacenado);
+        } catch (error) {
+            console.log(error)
+        };
+
+    } else if(tipoUsuario == "administrador") {
+        const existeUsuario = await Administrador.findOne({ correo });
+
+        if(existeUsuario) {
+            const error = new Error("usuario ya registrados");
+            return res.status(400).json({ msg: error.message });
+        };
+    
+        try {
+            const administradorUser = new Administrador(req.body);
+            administradorUser.toke = generarId();
+            const usuarioAlmacenado = await administradorUser.save();
+            res.json(usuarioAlmacenado);
+        } catch (error) {
+            console.log(error)
+        };
+    }
+}
 
 export { 
     registrar
