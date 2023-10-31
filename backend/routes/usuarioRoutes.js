3662b74338c076018f028fa619cf4584cdd5169c
sandro132 <mianveo@hotmail.com>
2023-10-30 22:44:49 -0500

    update the proyect

--- backend/routes/usuarioRoutes.js
+++ backend/routes/usuarioRoutes.js
@@ -1,9 +1,13 @@
 import express from "express";
-import { registrar } from "../controllers/userController.js";
+import { registrar, autenticar, confirmar, olvidePassword } from "../controllers/userController.js";
 
 const router = express.Router();
 
 // Autenticacion Registro y confirmacion de usuario
 router.post("/", registrar)
+router.post("/login", autenticar);
+router.get("/confirmar/:token", confirmar);
+router.post("/olvide-password", olvidePassword);
+
 
 export default router;
