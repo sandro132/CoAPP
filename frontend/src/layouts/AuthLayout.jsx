4eac76a25e7d77635e2c41962c945e4e73fb9214
MichiCaballero07 <michelcaballerogranado@gmail.com>
2023-11-03 12:27:47 -0500

    add new file

--- frontend/src/layouts/AuthLayout.jsx
+++ frontend/src/layouts/AuthLayout.jsx
@@ -3,8 +3,8 @@ import { Outlet } from "react-router-dom"
 const AuthLayout = () => {
     return (
       <>
-            <main className="container mx-auto mt-5 md:mt-20 p-5 md:flex md:justify-center">
-                <div className="md:w-2/3 lg:w-2/5">
+            <main className="container mx-auto mt-2 md:mt-20  md:flex justify-center ">
+                <div >
                     <Outlet />
                 </div>
            </main>
--- frontend/src/paginas/Login.jsx
+++ frontend/src/paginas/Login.jsx
@@ -1,130 +1,130 @@
-import {  useState } from 'react'
-import { Link} from "react-router-dom";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 import Alerta from "../components/Alerta";
 import clienteAxios from "../config/clienteAxios";
-import useAuth from '../hooks/useAuth';
-
+import useAuth from "../hooks/useAuth";
 
 const Login = () => {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [alerta, setAlerta] = useState({})
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [alerta, setAlerta] = useState({});
 
   const { setAuth } = useAuth();
-  
-  const handleSubmit = async e => {
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
-    if ([email, password].includes('')) {
+    if ([email, password].includes("")) {
       setAlerta({
-        msg: 'Todos los campos son obligatorios',
-        error: true
+        msg: "Todos los campos son obligatorios",
+        error: true,
       });
-      return
+      return;
     }
-  
 
     try {
-      const { data } = await clienteAxios.post('/usuarios/login', { email, password })
-      setAlerta({})
-      localStorage.setItem('token', data.token)
-      setAuth(data)
-
+      const { data } = await clienteAxios.post("/usuarios/login", {
+        email,
+        password,
+      });
+      setAlerta({});
+      localStorage.setItem("token", data.token);
+      setAuth(data);
     } catch (error) {
       setAlerta({
         msg: error.response.data.msg,
-        error: true
-      })
-
+        error: true,
+      });
     }
-  }
-  const { msg } = alerta
+  };
+  const { msg } = alerta;
 
   return (
-    <>
+    <div className="mx-8 max-w-lg">
       <div>
-        <img
-          className="display:flex align-items:center justify-content:center"
-          src="https://coally-images.s3.amazonaws.com/logo-coally-n.png"
-          alt="Logo Coally"
-          loading="lazy"
-          width="250"
-          height="115"
-          decoding="async"
-          data-nimg="1"
-        />
-      </div>
-      <div>
-        <h1 className="text-color:#393939 font-black text-4xl flex ">
-          ¡Bienvenido de nuevo!
-        </h1>
-      </div>
-
-      {msg && <Alerta alerta={alerta}/> }
-
-      <form
-        className="my-10 bg-white shadow rounder-lg p-10"
-        onSubmit={handleSubmit}
-      
-      >
-        <div className="my-5">
-          <label
-            className="uppercase text-gray-600 block text-xl font-bold"
-            htmlFor="email"
-          >
-            Correo
-          </label>
-          <input
-            id="email"
-            type="email"
-            placeholder="nombre@correo.com"
-            className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
+        <div>
+          <img
+            className="display:flex align-items:center justify-content:center"
+            src="https://coally-images.s3.amazonaws.com/logo-coally-n.png"
+            alt="Logo Coally"
+            loading="lazy"
+            width="250"
+            height="115"
+            decoding="async"
+            data-nimg="1"
           />
         </div>
-
-        <div className="my-5">
-          <label
-            className="uppercase text-gray-600 block text-xl font-bold"
-            htmlFor="password"
-          >
-            Contraseña
-          </label>
-          <input
-            id="password"
-            type="password"
-            placeholder="Introduce tu contraseña"
-            className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
+        <div>
+          <h1 className="text-color:#393939  font-black text-4xl flex ">
+            ¡Bienvenido de nuevo!
+          </h1>
         </div>
 
-        <input
-          type="submit"
-          value="Iniciar Sesión"
-          className="bg-orange-400 mb-5 w-full py-3 text-white uppercase font-bold rounded hover:cursor-pointer hover:bg-orange-500 transition-colors"
-        />
-      </form>
+        {msg && <Alerta alerta={alerta} />}
 
-      <nav className="lg:flex lg:justify-between center">
-        <Link
-          className="block text-center my-5 text-slate-500 uppercase text-sm"
-          to="registrar"
+        <form
+          className="my-10 bg-white shadow rounder-lg p-10 "
+          onSubmit={handleSubmit}
         >
-          ¿No tienes una cuenta?{" "}
-          <span className="text-sky-900">Registrate aquí</span>
-        </Link>
-        <Link
-          className="block text-center my-5 text-slate-500 uppercase text-sm"
-          to="/olvide-password"
-        >
-          Olvide Mi Contraseña{" "}
-        </Link>
-      </nav>
-    </>
+          <div className="my-2 ">
+            <label
+              className="uppercase text-gray-600 block text-xl font-bold"
+              htmlFor="email"
+            >
+              Correo
+            </label>
+            <input
+              id="email"
+              type="email"
+              placeholder="nombre@correo.com"
+              className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+
+          <div className="my-2">
+            <label
+              className="uppercase text-gray-600 block text-xl font-bold"
+              htmlFor="password"
+            >
+              Contraseña
+            </label>
+            <input
+              id="password"
+              type="password"
+              placeholder="Introduce tu contraseña"
+              className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+
+          <input
+            type="submit"
+            value="Iniciar Sesión"
+            className="bg-orange-400 mb-2 w-full py-3 text-white uppercase font-bold rounded hover:cursor-pointer hover:bg-orange-500 transition-colors"
+          />
+        </form>
+
+        <nav className="lg:flex lg:justify-between center">
+          <Link
+            className="block text-center my-5 text-slate-500 uppercase text-sm"
+            to="registrar"
+          >
+            ¿No tienes una cuenta?{" "}
+            <span className="text-sky-900">Registrate aquí</span>
+          </Link>
+          <Link
+            className="block text-center my-5 text-slate-500 uppercase text-sm"
+            to="/olvide-password"
+          >
+            Olvide Mi Contraseña{" "}
+          </Link>
+        </nav>
+      </div>
+    </div>
   );
-}
+};
 
-export default Login
+export default Login;
