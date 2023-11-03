448e5600f198e1a34457a1d79436aec55470c140
MichiCaballero07 <michelcaballerogranado@gmail.com>
2023-11-03 10:43:18 -0500

    add new files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,7 @@ import OlvidePassword from "./paginas/OlvidePassword"
 import NuevoPassword from "./paginas/NuevoPassword"
 import ConfirmarCuenta from "./paginas/ConfirmarCuenta"
 import {AuthProvider} from './context/AuthProvider'
+import PaginaPrincipal from "./paginas/PaginaPrincipal"
 
 
 console.log(import.meta.env.VITE_BACKEND_URL)
@@ -16,6 +17,7 @@ function App() {
         <Routes>
           <Route path="/" element={<AuthLayout />}>
             <Route index element={<Login />} />
+            <Route path="pagina-principal" element={<PaginaPrincipal/>} />
             <Route path="registrar" element={<Registrar />} />
             <Route path="olvide-password" element={<OlvidePassword />} />
             <Route path="olvide-password/:token" element={<NuevoPassword />} />
