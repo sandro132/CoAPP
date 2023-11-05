0c55fecdacfd9de84eaf3322a216660326291186
Bluebee45 <santi72143@gmail.com>
2023-11-04 19:03:00 -0500

    update files

--- frontend/src/components/BotonActualizar.jsx
+++ frontend/src/components/BotonActualizar.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 const Boton = () => {
     return (
-        <div className="">
-             <button>
+    <div className="justify-center items-center bg-blue-800 p-2 text-white rounded-2 hover:bg-blue-700 hover:scale-110">
+        <button>
             Actualizar datos
         </button>
-        </div>
+    </div>
        
     )
 }
