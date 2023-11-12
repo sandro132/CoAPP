b05653b6dd4deb848bf021b1c81d574247d7579d
sandro132 <mianveo@hotmail.com>
2023-11-12 13:06:31 -0500

    finally fix main backend

---
+++ backend/models/Usuarios.js
@@ -0,0 +1,59 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+
+const usuarioSchema = mongoose.Schema(
+    {
+        typeUser: {
+            type: String,
+            required: false,
+            enum: ["profesional", "empresa", "administrador"], // Valores permitidos
+        },
+        name: {
+            type: String,
+            require: true,
+            trim: true // Elimina espacios de incio y fin
+        },
+        
+        password: {
+            type: String,
+            require: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            require: true,
+            trim: true,
+            unique: true //Garantiza que no sea un correo ya registrado
+        },
+
+        
+        token: {
+            type: String
+        },
+        confirmado: {
+            type: Boolean,
+            default: false
+        },
+    },
+    {
+        timestamps: true,
+    },
+);
+
+
+// Este codigo se ejecutara antes de almacenar en la base de datos
+usuarioSchema.pre("save", async function(next) {
+    if(!this.isModified("contraseña")) { // al llamar la funcion no se esta modificando la contraseña, no se ejecutara el hasheo
+        next()
+    };
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+});
+
+// Verifica si la contraseña escrita coincide con la de la base de datos
+usuarioSchema.methods.comprobarContraseña = async function (contraseñaFormulario) {
+    return await bcrypt.compare(contraseñaFormulario, this.password)
+};
+
+const Usuario = mongoose.model("Usuarios", usuarioSchema)
+export default Usuario
