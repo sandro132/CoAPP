9b73a018daa6014130a66077c0d800acbf8d4e24
MichiCaballero07 <michelcaballerogranado@gmail.com>
2023-11-13 00:49:33 -0500

    update files

--- frontend/src/components/Alerta.jsx
+++ frontend/src/components/Alerta.jsx
@@ -1,6 +1,6 @@
 const Alerta = ({alerta}) => {
     return (
-        <div className={`${alerta.error ? 'from-orange-400 to-orange-600' : 'from-sky-400 to - sky - 600'} bg-gradient-to-br text-cente p-3 rounded-xl uppercase text-white font-bold text-sm my-10 `}>
+        <div className={`${alerta.error ? 'from-orange-400 to-orange-600' : 'from-sky-800 to-sky-900'}  text-center p-3 rounded-xl uppercase text-white font-bold text-sm my-10 `}>
             {alerta.msg}
         </div>
     )
