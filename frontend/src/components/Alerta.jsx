d2f284146950d4d47a1f18e304dd4602a4baf159
sandro132 <mianveo@hotmail.com>
2023-11-13 19:25:24 -0500

    s

--- frontend/src/components/Alerta.jsx
+++ frontend/src/components/Alerta.jsx
@@ -5,7 +5,7 @@ const Alerta = ({alerta}) => {
           alerta.error
             ? "from-orange-500 to-orange-500"
             : "from-sky-800 to-sky-800"
-        }  bg-gradient-to-r text-center p-3  uppercase text-white font-bold text-sm my-10  mb-2 w-full py-3   rounded `}
+        }  bg-gradient-to-br text-center p-3  uppercase text-white font-bold text-sm my-10  mb-2 w-full py-3   rounded `}
       >
         {alerta.msg}
       </div>
